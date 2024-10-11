31dda7347ca72528f69caa5a7e6506fce3f76d9f
Eiichi Sayama <eiichimouse@outlook.jp>
2024-10-11 22:22:24 +0900

    feat:Get input from user

--- main.go
+++ main.go
@@ -3,34 +3,55 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type model struct {
-	choices  []string         // items on the to-do list
-	cursor   int              // which to-do list item our cursor is pointing at
-	selected map[int]struct{} // which to-do items are selected
+	// choices  []string         // items on the to-do list
+	// cursor   int              // which to-do list item our cursor is pointing at
+	// selected map[int]struct{} // which to-do items are selected
+	searchPath   []string
+	searchString textinput.Model
 }
 
 func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "Search keyword"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 20
+
+	username := os.Getenv("WindowsUserName")
+	var searchPathString strings.Builder
+	searchPathString.WriteString("C:\\Users\\")
+	searchPathString.WriteString(username)
+	searchPathString.WriteString("\\AppData\\Local\\Google\\Chrome\\User Data")
+
 	return model{
 		// Our to-do list is a grocery list
-		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+		// choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
 
 		// A map which indicates which choices are selected. We're using
 		// the  map like a mathematical set. The keys refer to the indexes
 		// of the `choices` slice, above.
-		selected: make(map[int]struct{}),
+		// selected: make(map[int]struct{}),
+		searchPath:   []string{searchPathString.String()},
+		searchString: ti,
 	}
 }
 
 func (m model) Init() tea.Cmd {
 	// Just return `nil`, which means "no I/O right now, please."
-	return nil
+	return textinput.Blink
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	// fmt.Println("msg : ", msg)
 	switch msg := msg.(type) {
 
 	// Is it a key press?
@@ -40,66 +61,64 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 
 		// These keys should exit the program.
-		case "ctrl+c", "q":
+		case "ctrl+c":
+			fmt.Println("Bye bye!")
 			return m, tea.Quit
 
-		// The "up" and "k" keys move the cursor up
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		// The "down" and "j" keys move the cursor down
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
-		// The "enter" key and the spacebar (a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
+			// The "enter" key and the spacebar (a literal space) toggle
+			// the selected state for the item that the cursor is pointing at.
 		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
+			fmt.Println("at the first part of enter")
+			// reader := bufio.NewReader(os.Stdin)
+			// text, _ := reader.ReadString('\n')
+			// m.searchString = text
+			fmt.Println("at the last part of enter")
+			// _, ok := m.selected[m.cursor]
+			// if ok {
+			// 	delete(m.selected, m.cursor)
+			// } else {
+			// 	m.selected[m.cursor] = struct{}{}
+			// }
 		}
 	}
 
 	// Return the updated model to the Bubble Tea runtime for processing.
 	// Note that we're not returning a command.
-	return m, nil
+	m.searchString, cmd = m.searchString.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
 	// The header
-	s := "What should we buy at the market?\n\n"
+	s := "Below string is being searched... \n\n"
+	// s := "What should we buy at the market?\n\n"
 
 	// Iterate over our choices
-	for i, choice := range m.choices {
+	// for i, choice := range m.choices {
 
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
+	// 	// Is the cursor pointing at this choice?
+	// 	cursor := " " // no cursor
+	// 	if m.cursor == i {
+	// 		cursor = ">" // cursor!
+	// 	}
 
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
+	// 	// Is this choice selected?
+	// 	checked := " " // not selected
+	// 	if _, ok := m.selected[i]; ok {
+	// 		checked = "x" // selected!
+	// 	}
 
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-	}
+	// 	// Render the row
+	// 	s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	// }
+	s += fmt.Sprintf("%s\n", m.searchString.View())
 
 	// The footer
 	s += "\nPress q to quit.\n"
 
 	// Send the UI for rendering
-	return s
+	return fmt.Sprintf("%s\n", m.searchString.View())
+	// return s
 }
 
 func main() {
