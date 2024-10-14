398e8817adc6aabd2f86d82215056e8d25cbcb2f
Eiichi Sayama <eiichimouse@outlook.jp>
2024-10-14 12:03:30 +0900

    chore:Delete unused code

--- main.go
+++ main.go
@@ -114,8 +114,6 @@ func getAllBookmarkFilePath() []string {
 }
 
 func (m model) Init() tea.Cmd {
-
-	// Just return `nil`, which means "no I/O right now, please."
 	return textinput.Blink
 }
 
@@ -124,19 +122,14 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	switch msg := msg.(type) {
 
-	// Is it a key press?
 	case tea.KeyMsg:
 
-		// Cool, what was the actual key pressed?
 		switch msg.String() {
 
-		// These keys should exit the program.
 		case "ctrl+c":
 			fmt.Println("Bye bye!")
 			return m, tea.Quit
 
-			// The "enter" key and the spacebar (a literal space) toggle
-			// the selected state for the item that the cursor is pointing at.
 		case "enter", " ":
 			var searchWord []string
 			for _, v := range m.searchString.Value() {
@@ -152,8 +145,6 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	}
 
-	// Return the updated model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
 	m.searchString, cmd = m.searchString.Update(msg)
 	return m, cmd
 }
@@ -199,16 +190,12 @@ func getPathName() string {
 }
 
 func (m model) View() string {
-	// The header
 	s := "Below string is being searched... \n\n"
 	s += fmt.Sprintf("%s\n", m.searchString.View())
 
-	// The footer
 	s += "\nPress q to quit.\n"
 
-	// Send the UI for rendering
 	return fmt.Sprintf("%s\n", m.searchString.View())
-	// return s
 }
 
 func checkError(e error) {
