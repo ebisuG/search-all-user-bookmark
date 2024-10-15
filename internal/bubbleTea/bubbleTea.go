c6763ae85b7621f61fd20bcb1b8f481a41836100
Eiichi Sayama <eiichimouse@outlook.jp>
2024-10-15 10:58:28 +0900

    refactor:Split code in main file into bubbleTea and util package

---
+++ internal/bubbleTea/bubbleTea.go
@@ -0,0 +1,89 @@
+package bubbleTea
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/ebisuG/search-all-user-bookmark/internal/util"
+)
+
+type model struct {
+	searchPath   []string
+	searchString textinput.Model
+	allUrl       []util.InfoDisplayed
+}
+
+// type InfoDisplayed struct {
+// 	name string
+// 	url  string
+// }
+
+func InitialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "Search keyword"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 20
+
+	fmt.Println("Press Ctrl + c when you quit")
+	fmt.Println("Reading all bookmark files...")
+	bookmarkFilesPath := util.GetAllBookmarkFilePath()
+	var allData []util.InfoDisplayed
+	allData = append(allData, util.ReadBookmarkFile(util.GetPathName()+"\\Default\\Bookmarks")...)
+	for _, v := range bookmarkFilesPath {
+		allData = append(allData, util.ReadBookmarkFile(v)...)
+	}
+	fmt.Println("Finish reading bookmark files...")
+	return model{
+		searchPath:   []string{util.GetPathName()},
+		searchString: ti,
+		allUrl:       allData,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+
+		switch msg.String() {
+
+		case "ctrl+c":
+			fmt.Println("Bye bye!")
+			return m, tea.Quit
+
+		case "enter", " ":
+			var searchWord []string
+			for _, v := range m.searchString.Value() {
+				searchWord = append(searchWord, string(v))
+			}
+			var display []util.InfoDisplayed
+			display = util.FilterByString(m.allUrl, strings.Join(searchWord, ""))
+
+			for _, v := range display {
+				fmt.Println(v.Name, " : ", v.Url, "")
+			}
+			fmt.Println("")
+		}
+	}
+
+	m.searchString, cmd = m.searchString.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	s := "Below string is being searched... \n\n"
+	s += fmt.Sprintf("%s\n", m.searchString.View())
+
+	s += "\nPress q to quit.\n"
+
+	return fmt.Sprintf("%s\n", m.searchString.View())
+}
---
+++ internal/util/util.go
@@ -0,0 +1,127 @@
+package util
+
+import (
+	"encoding/json"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type InfoDisplayed struct {
+	Name string
+	Url  string
+}
+
+type ParentJson struct {
+	Checksum     string `json:"checksum"`
+	Roots        Roots  `json:"roots"`
+	SyncMetadata string `json:"sync_metadata"`
+	Version      int    `json:"version"`
+}
+
+type Roots struct {
+	BookmarkBar BookmarkBar `json:"bookmark_bar"`
+}
+
+type BookmarkBar struct {
+	Children []Child `json:"children"`
+	Other    Child   `json:"other"`
+	Synced   Child   `json:"synced"`
+}
+
+type Child struct {
+	Children     []Child   `json:"children"`
+	DateAdded    string    `json:"date_added"`
+	DateLastUsed string    `json:"date_last_used"`
+	Guid         string    `json:"guid"`
+	Id           string    `json:"id"`
+	MetaInfo     Meta_info `json:"meta_info"`
+	Name         string    `json:"name"`
+	Type         string    `json:"type"`
+	Url          string    `json:"url"`
+}
+
+type Meta_info struct {
+	PowerBookmarkMeta string `json:"power_bookmark_meta"`
+}
+
+type SettingFile struct {
+	UserName string `json:"username"`
+}
+
+func ReadBookmarkFile(path string) []InfoDisplayed {
+	data, err := os.ReadFile(path)
+	CheckError(err)
+	var bookmarks ParentJson
+	json.Unmarshal(data, &bookmarks)
+	var display []InfoDisplayed
+	for i := 0; i < len(bookmarks.Roots.BookmarkBar.Children); i++ {
+		bookmark := bookmarks.Roots.BookmarkBar.Children[i]
+		display = append(display, GetChildren(bookmark)...)
+	}
+	return display
+}
+
+func GetAllBookmarkFilePath() []string {
+	var bookmarksFilePath []string
+	pathName := GetPathName()
+	files, err := os.ReadDir(pathName)
+	if err != nil {
+		panic(err)
+	}
+	r, _ := regexp.Compile("^Profile [0-9]*")
+
+	for _, v := range files {
+		match := r.MatchString(v.Name())
+		if v.IsDir() && match {
+			bookmarksFilePath = append(bookmarksFilePath, pathName+"\\"+v.Name()+"\\Bookmarks")
+		}
+	}
+	return bookmarksFilePath
+}
+
+func GetChildren(c Child) []InfoDisplayed {
+	var result []InfoDisplayed
+	if c.Type == "folder" {
+		for _, v := range c.Children {
+			result = append(result, GetChildren(v)...)
+		}
+	} else {
+		var pair InfoDisplayed
+		pair.Name, pair.Url = c.Name, c.Url
+		result = append(result, pair)
+	}
+	return result
+}
+
+func FilterByString(pairs []InfoDisplayed, search string) []InfoDisplayed {
+	var result []InfoDisplayed
+	for _, v := range pairs {
+		isInName := strings.Contains(v.Name, search)
+		isInUrl := strings.Contains(v.Url, search)
+		if isInName || isInUrl {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func GetPathName() string {
+	data, err := os.ReadFile("./settings.json")
+	if err != nil {
+		panic(err)
+	}
+	var settings SettingFile
+	json.Unmarshal(data, &settings)
+	var searchPathString strings.Builder
+	searchPathString.WriteString("C:\\Users\\")
+	searchPathString.WriteString(settings.UserName)
+	searchPathString.WriteString("\\AppData\\Local\\Google\\Chrome\\User Data")
+	return searchPathString.String()
+}
+
+func CheckError(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
