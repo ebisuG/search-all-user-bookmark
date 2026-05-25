064a850c1b32cbf138e539718ab698995807096a
Eiichi Sayama <eiichimouse@outlook.jp>
2026-05-25 22:53:41 +0900

    feat: print hello world

---
+++ v2/cmd/root.go
@@ -0,0 +1,24 @@
+package root
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "sab",
+	Short: "sab means search all user's bookmark",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello world")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		os.Exit(1)
+	}
+}
---
+++ v2/main.go
@@ -0,0 +1,7 @@
+package main
+
+import root "github.com/ebisuG/search-all-user-bookmark/v2/cmd"
+
+func main() {
+	root.Execute()
+}
