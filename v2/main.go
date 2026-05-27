e157a8453e0a486530c73340c8ee404a9812cc25
Eiichi Sayama <eiichimouse@outlook.jp>
2026-05-28 00:27:50 +0900

    refactor: depend on config.Loader in main.go
    Add NewLoader to return Loader interface instead of concrete implementation.

--- v2/main.go
+++ v2/main.go
@@ -1,7 +1,19 @@
 package main
 
-import root "github.com/ebisuG/search-all-user-bookmark/v2/cmd"
+import (
+	"fmt"
+
+	root "github.com/ebisuG/search-all-user-bookmark/v2/cmd"
+	"github.com/ebisuG/search-all-user-bookmark/v2/internal/config"
+)
 
 func main() {
+	loader := NewLoader()
+	cfg := loader.Load()
+	fmt.Println(cfg)
 	root.Execute()
+}
+
+func NewLoader() config.Loader {
+	return &config.Config{}
 }
